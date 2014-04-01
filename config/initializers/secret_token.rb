4e9445b6b29e29ea70ac82506510ab22167b30da
fenmarel <jonny.p.tatoes@gmail.com>
2014-04-01 10:23:13 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoidRage::Application.config.secret_key_base = '649de87fd0a2932e57921beb8beff51ec0f964541e9a24d9e063bdbf1d9735d83798d63007adae71e771cf3940d6fa755da9ba22b334a223392ccb477b3d7880'
